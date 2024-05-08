cd7bf3fad952c853a69c697329c765eb2c9385d6
Vivek Tiwari <vivekcs0114@gmail.com>
2024-05-08 23:27:07 +0530

    create job in folder

--- jobs/job.groovy
+++ jobs/job.groovy
@@ -1,27 +1,25 @@
 folder("ABCDE") {
     displayName("ABCDE")
     description('Folder for project ABCDE')
+}
 
-
-    job('demo') {
-        steps {
-            shell('echo Hello World!')
-        }
-    }
-
-    pipelineJob('github-demo') {
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            github('vivekcs0114/jenkins')
-                        }
-                    }
-                }
-                scriptPath('pipeline/pipeline.groovy')
-            }
-        }
+job('ABCDE/demo') {
+    steps {
+        shell('echo Hello World!')
     }
 }
 
+pipelineJob('ABCDE/github-demo') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('vivekcs0114/jenkins')
+                    }
+                }
+            }
+            scriptPath('pipeline/pipeline.groovy')
+        }
+    }
+}
