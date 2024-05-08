9040bf6f78923b490fc4bb4dc0cff2f16af3aadb
Vivek Tiwari <vivekcs0114@gmail.com>
2024-05-08 23:03:02 +0530

    add config

--- jobs/job.groovy
+++ jobs/job.groovy
@@ -3,3 +3,18 @@ job('demo') {
         shell('echo Hello World!')
     }
 }
+
+pipelineJob('github-demo') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('vivekcs0114/jenkins')
+                    }
+                }
+            }
+            scriptPath('pipeline/pipeline.groovy')
+        }
+    }
+}
