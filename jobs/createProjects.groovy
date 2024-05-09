45a544d8cdd7c104b2b419daa3def115dfc26c56
Vivek Tiwari <vivekcs0114@gmail.com>
2024-05-09 18:08:01 +0530

    add params

--- jobs/createProjects.groovy
+++ jobs/createProjects.groovy
@@ -28,13 +28,11 @@ def createMultiBranchPipeline(project, repo, folder, url, filePath) {
                 }
             }
         }
-        properties([
-            parameters([
-                string(name: 'submodule', defaultValue: ''),
-                string(name: 'submodule_branch', defaultValue: ''),
-                string(name: 'commit_sha', defaultValue: ''),
-            ])
-        ])
+        parameters {
+            string(name: 'submodule', defaultValue: ''),
+            string(name: 'submodule_branch', defaultValue: ''),
+            string(name: 'commit_sha', defaultValue: ''),
+        }
         factory {
             workflowBranchProjectFactory {
                 scriptPath(filePath)
