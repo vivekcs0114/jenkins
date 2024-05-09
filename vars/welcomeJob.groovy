1175ffabdbe183b2567ca1dbf3f93d7c14667770
Vivek Tiwari <vivekcs0114@gmail.com>
2024-05-09 21:16:07 +0530

    add params

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,3 +1,8 @@
 def call(String name = 'User') {
+    properties([
+        parameters([
+            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+        ])
+    ])
     echo "Welcome, ${name}."
 }
